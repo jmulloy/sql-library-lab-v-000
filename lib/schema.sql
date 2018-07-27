5d8f2948eb463eab07a6534dd476031a17324242
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-27 14:31:38 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -6,20 +6,24 @@ CREATE TABLE series (
 );
 
 CREATE TABLE sub_genre (
+  id INTEGER PRIMARY KEY,
   name TEXT
 );
 
 CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
   name TEXT
 );
 
 CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
   series TEXT
 );
 
 CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
   species TEXT,
