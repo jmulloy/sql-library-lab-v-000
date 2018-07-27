65d7fbc0c093a61e0e7548a6ba88803e22146de3
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-27 15:46:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT value, count "
+  "SELECT value, count FROM characters"
 end
 
 def select_name_and_series_subgenres_of_authors
